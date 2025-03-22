dd682263cc4264e291daa21b1aee990ceb0b2e05
TravelingGithub <jaqsuperstore@gmail.com>
2025-03-22 10:56:47 -0500

    few minor fixes

--- bruteforce.py
+++ bruteforce.py
@@ -10,25 +10,24 @@ def attempt_extract(zf_handle, password):
 def main():
     print("Bruteforce...")
     attempts = 0
-	
-	#Open Zip
+    
+    #Open Zip
     with ZipFile('file.zip') as zf:
         with open('rockyou.txt', 'rb') as f: 
-	
-	#Iterate rockyou.txt
+    
+            #Iterate rockyou.txt
             for p in f:
                 password = p.strip()
                 attempts += 1
                 
-		#prints every 1000 attempts
+                #prints every 1000 attempts
                 if attempts % 1000 == 0:
                     print(f"[*] Tried {attempts} passwords...")
                 
-		#Password Found
+                #Password Found
                 if attempt_extract(zf, password):
                     print(f"PWN: {password.decode()}")
                     exit(0)
-
     print("Oh No..")
 
 if __name__ == "__main__":
